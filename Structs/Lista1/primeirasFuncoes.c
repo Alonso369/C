cd34bed8e739e69267f1d56e651e893ffbc9c1de
Alonso Kraemer <93839204+Alonso369@users.noreply.github.com>
2023-08-12 00:25:16 -0300

    primieras funções

---
+++ Structs/Lista1/primeirasFuncoes.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct dosesDeVacina{
+    char nome[40];
+    char cidade[30];
+    int idade;
+};
+
+typedef struct dosesDeVacina Doses;
+
+Doses cadastroPaciente(int i){
+    Doses d;
+    printf("Informe o nome do paciente: ");
+    gets(d.nome);
+    printf("Informe a cidade do paciente: ");
+    gets(d.cidade);
+    printf("Infomre a idade do paciente: ");
+    scanf("%d", &d.idade);
+    return d;
+}
+
+void imprimeDados(Doses doses){
+    printf("Nome: %s\n", doses.nome);
+    printf("Cidade: %s\n", doses.cidade);
+    printf("Idade: %d\n", doses.idade);
+}
+
+
+int main()
+{
+    Doses doses[5];
+    for(int i = 0; i < 2; i++){
+        doses[i] = cadastroPaciente(i);
+        setbuf(stdin, NULL);
+    }
+
+    printf("\nDados Informados: \n");
+
+    for(int i = 0; i < 2; i++){
+        imprimeDados(doses[i]);
+        printf("\n");
+    }
+    return 0;
+}
